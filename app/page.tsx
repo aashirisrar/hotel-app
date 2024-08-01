0d0f509d715fb41ed02aa44ef8c6dc932f91ead0
John Doe <aashirisrar968@gmail.com>
2024-08-01 13:03:43 +0500

    fetching listings from database and displaying them

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,30 @@
-import Image from "next/image";
+import ListingCard from "@/components/listings/ListingCard";
+import { getCurrentUser } from "@/app/actions/getCurrentUser";
+import Container from "@/components/Container";
+import EmptyState from "@/components/EmptyState";
+import getListings from "@/app/actions/getListings";
 
-export default function Home() {
-  return <main>Hello World</main>;
+export default async function Home() {
+  const listings = await getListings();
+  const currentUser = await getCurrentUser();
+
+  if (listings.length === 0) {
+    return <EmptyState showReset />;
+  }
+
+  return (
+    <Container>
+      <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+        {listings.map((listing: any) => {
+          return (
+            <ListingCard
+              currentUser={currentUser}
+              key={listing.id}
+              data={listing}
+            />
+          );
+        })}
+      </div>
+    </Container>
+  );
 }
