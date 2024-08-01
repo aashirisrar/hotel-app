f233a24a915cb6d84fa76519c2bbcd6b5636ff37
John Doe <aashirisrar968@gmail.com>
2024-08-01 18:02:11 +0500

    added option to add reservations

--- app/trips/page.tsx
+++ app/trips/page.tsx
@@ -12,13 +12,14 @@ const TripsPage = async () => {
 
   const reservations = await getReservations({ userId: currentUser.id });
 
-  if (reservations.length === 0)
+  if (reservations.length === 0) {
     return (
       <EmptyState
         title="No trips found"
         subtitle="Looks like you haven't reserved any trips."
       />
     );
+  }
 
   return <TripsClient reservations={reservations} currentUser={currentUser} />;
 };
